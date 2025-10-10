c568d047e5e545a381a9e5f7c86900afb93cd6d5
Derick Axel Lagunes Ramirez <dericklagunes@utez.edu.mx>
2025-10-10 13:24:17 -0600

    InfoCards nuevas

--- app/src/main/java/mx/edu/utez/calculadoramvvm/ui/components/InfoCard.kt
+++ app/src/main/java/mx/edu/utez/calculadoramvvm/ui/components/InfoCard.kt
@@ -1,16 +1,23 @@
+package mx.edu.utez.calculadoramvvm.ui.components
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import mx.edu.utez.calculadoramvvm.viewmodel.MenuViewModel
 
 @Composable
 fun InfoCard(
     title: String,
-    description: String
+    description: String,
+    modifier: Modifier,
+    viewModel: MenuViewModel
 ) {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(6.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
